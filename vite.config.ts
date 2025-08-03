64add39ef50b76ab8a33a955eb86a16b15b77f94
Kritik Kaushal <kritikk882@gmail.com>
2025-08-03 11:38:14 -0400

    Fix base path for custom domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/newsleepline-canada/' : '/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
